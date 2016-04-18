26764943ac61c25aef54a325534d7c63e4ee6824
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-04-18 16:22:56 +0200

    [FIX] sale_order_create_event: No create stock move where is product service.

--- sale_order_create_event/models/project.py
+++ sale_order_create_event/models/project.py
@@ -101,10 +101,13 @@ class ProjectTask(models.Model):
             task.project_id = project.id
             new_account.date = parent_account.date
             project.date = parent_account.date
+            sale_name = task.service_project_sale_line.order_id.name
+            parent_account.write({'name': sale_name,
+                                  'event_id': False})
         return task
 
     def _create_new_account_project_by_task(self, task, parent_account, code):
-        vals = {'name': (task.project_id.name + ': ' +
+        vals = {'name': (task.service_project_sale_line.order_id.name + ': ' +
                          task.service_project_sale_line.name),
                 'use_tasks': True,
                 'type': 'contract',
