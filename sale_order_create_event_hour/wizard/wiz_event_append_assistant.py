aa18e60557976c2626567eedb6a499b7c3e90e8b
oihane <oihanecruce@gmail.com>
2016-04-18 18:16:45 +0200

    [IMP] sale_order_create_event_hour: fixed PEP8

--- sale_order_create_event_hour/wizard/wiz_event_append_assistant.py
+++ sale_order_create_event_hour/wizard/wiz_event_append_assistant.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) 2016 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import fields, models, api, _, exceptions
+from openerp import fields, models, api, _
 
 
 class WizEventAppendAssistant(models.TransientModel):
