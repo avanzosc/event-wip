742cdc3bd8a89a6e3943510814e91028a4bbf6f8
oihane <oihanecruce@gmail.com>
2016-04-21 09:55:01 +0200

    [IMP] event_track_assistant: fixed author

--- event_track_assistant/__openerp__.py
+++ event_track_assistant/__openerp__.py
@@ -5,8 +5,7 @@
     "name": "Event Track Assistant",
     "version": "8.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Avanzosc, S.L., "
-              "Odoo Community Association (OCA)",
+    "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
     "contributors": [
         "Ana Juaristi <anajuaristi@avanzosc.es>",
