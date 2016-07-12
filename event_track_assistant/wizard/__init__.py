f6fa507ab28a84dfde66dd4f0bfd37523a58ede2
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-07-12 15:36:42 +0200

    [IMP] event_track_assistant: New wizard for confirm presences in events.

--- event_track_assistant/wizard/__init__.py
+++ event_track_assistant/wizard/__init__.py
@@ -3,4 +3,5 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import wiz_event_append_assistant
 from . import wiz_event_delete_assistant
+from . import wiz_event_confirm_assistant
 from . import wiz_change_session_hour
