ae93451e6811d7f970bb6ffa58d26389845f9874
agaldona <ainaragaldona@avanzosc.es>
2016-07-14 13:08:51 +0200

    [ADD] event_track_info: new module

---
+++ event_track_info/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Event Track Info",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Ainara Galdona <ainaragaldona@avanzosc.es>",
+    ],
+    "category": "Event Management",
+    "depends": [
+        "website_event_track",
+    ],
+    "data": [
+        "views/event_track_view.xml",
+    ],
+    "installable": True,
+}
