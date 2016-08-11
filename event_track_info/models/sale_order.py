efd1fd908ba72533e3360c93204580b39fee7865
oihane <oihanecruce@gmail.com>
2016-08-11 15:45:51 +0200

    [IMP] sale_order_create_event: changed test in order to show if they are correct

--- event_track_info/models/sale_order.py
+++ event_track_info/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) 2016 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, api, _
+from openerp import models, api
 
 
 class SaleOrder(models.Model):
@@ -11,22 +11,24 @@ class SaleOrder(models.Model):
     def action_button_confirm(self):
         template_obj = self.env['product.event.track.template']
         res = super(SaleOrder, self).action_button_confirm()
-        for sale in self:
-            for line in sale.order_line:
-                if line.event_id and line.product_id.event_track_template_ids:
-                    sequence = 0
-                    for track in line.event_id.track_ids:
-                        sequence += 1
-                        cond = [('product_id', '=', line.product_id.id),
-                                ('sequence', '=', sequence)]
-                        template = template_obj.search(cond, limit=1)
-                        if template:
-                            name = "{} {}: {}".format(_('Session'), sequence,
-                                                      template.name)
-                            vals = {'name': name,
-                                    'url': template.url,
-                                    'description': template.html_info,
-                                    'planification': template.planification,
-                                    'resolution': template.resolution}
-                            track.write(vals)
+        for line in self.mapped('order_line').filtered(
+                lambda l: l.event_id and
+                l.product_id.event_track_template_ids):
+            sequence = 0
+            for track in line.event_id.track_ids:
+                sequence += 1
+                cond = [('product_id', '=', line.product_id.id),
+                        ('sequence', '=', sequence)]
+                template = template_obj.search(cond, limit=1)
+                if template:
+                    # There is a problem with acuted letters, works with ascii
+                    name = "{} {}: {}".format('Session', sequence,
+                                              template.name)
+                    vals = {'name': name,
+                            'url': template.url,
+                            'description': template.html_info,
+                            'planification': template.planification,
+                            'resolution': template.resolution}
+                    track.write(vals)
+                sequence += 1
         return res
