914d0f36d0c8ebf3a84c061aaa3d0ea5323121c2
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-08-18 15:56:40 +0200

    [FIX] event_track_info: Fix bug when in sales order lines there is a nonrecurring service.

--- event_track_info/models/sale_order.py
+++ event_track_info/models/sale_order.py
@@ -11,24 +11,26 @@ class SaleOrder(models.Model):
     def action_button_confirm(self):
         template_obj = self.env['product.event.track.template']
         res = super(SaleOrder, self).action_button_confirm()
-        for line in self.mapped('order_line').filtered(
-                lambda l: l.event_id and
-                l.product_id.event_track_template_ids):
-            sequence = 0
-            for track in line.event_id.track_ids:
-                sequence += 1
-                cond = [('product_id', '=', line.product_id.id),
-                        ('sequence', '=', sequence)]
-                template = template_obj.search(cond, limit=1)
-                if template:
-                    # There is a problem with acuted letters, works with ascii
-                    name = "{} {}: {}".format('Session', sequence,
-                                              template.name)
-                    vals = {'name': name,
-                            'url': template.url,
-                            'description': template.html_info,
-                            'planification': template.planification,
-                            'resolution': template.resolution}
-                    track.write(vals)
-                sequence += 1
+        for sale in self:
+            for line in sale.mapped('order_line').filtered(
+                    lambda l: l.event_id and
+                    l.product_id.event_track_template_ids):
+                sequence = 0
+                for track in line.event_id.track_ids:
+                    sequence += 1
+                    cond = [('product_id', '=', line.product_id.id),
+                            ('sequence', '=', sequence)]
+                    template = template_obj.search(cond, limit=1)
+                    if template:
+                        # There is a problem with acuted letters, works
+                        # with ascii
+                        name = "{} {}: {}".format('Session', sequence,
+                                                  template.name)
+                        vals = {'name': name,
+                                'url': template.url,
+                                'description': template.html_info,
+                                'planification': template.planification,
+                                'resolution': template.resolution}
+                        track.write(vals)
+                    sequence += 1
         return res
