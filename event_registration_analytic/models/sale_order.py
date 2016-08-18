6f13946610745e348816e156c1c575d3ccd7ef8c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-08-18 15:53:19 +0200

    [FIX] event_registration_analytic: Fix bug when in sales order lines there is a nonrecurring service.

--- event_registration_analytic/models/sale_order.py
+++ event_registration_analytic/models/sale_order.py
@@ -12,13 +12,14 @@ class SaleOrder(models.Model):
         project_obj = self.env['project.project']
         event_obj = self.env['event.event']
         res = super(SaleOrder, self).action_button_confirm()
-        cond = [('analytic_account_id', '=', self.project_id.id)]
-        project = project_obj.search(cond, limit=1)
-        cond = [('project_id', '=', project.id)]
-        events = event_obj.search(cond)
-        for event in events:
-            tickets = event.event_ticket_ids.filtered(
-                lambda x: x.product_id.id ==
-                self.env.ref('event_sale.product_product_event').id)
-            tickets.unlink()
+        for sale in self.filtered(lambda x: x.project_id):
+            cond = [('analytic_account_id', '=', sale.project_id.id)]
+            project = project_obj.search(cond, limit=1)
+            cond = [('project_id', '=', project.id)]
+            events = event_obj.search(cond)
+            for event in events:
+                tickets = event.event_ticket_ids.filtered(
+                    lambda x: x.product_id.id ==
+                    self.env.ref('event_sale.product_product_event').id)
+                tickets.unlink()
         return res
