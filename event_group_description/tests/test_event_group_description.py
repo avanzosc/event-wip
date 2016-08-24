a3f6126546db5f3b5d06f1f74568c7cb50d69a0f
oihane <oihanecruce@gmail.com>
2016-08-24 14:32:06 +0200

    [IMP] event_group_description: improved code (#67)

--- event_group_description/tests/test_event_group_description.py
+++ event_group_description/tests/test_event_group_description.py
@@ -1,126 +1,48 @@
 # -*- coding: utf-8 -*-
-# (c) 2025 Alfredo de la Fuente - AvanzOSC
+# © 2016 Alfredo de la Fuente - AvanzOSC
+# © 2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-import openerp.tests.common as common
+
+from openerp.addons.sale_order_create_event.tests.\
+    test_sale_order_create_event import TestSaleOrderCreateEvent
 
 
-class TestEventGroupDescription(common.TransactionCase):
+class TestEventGroupDescription(TestSaleOrderCreateEvent):
 
     def setUp(self):
         super(TestEventGroupDescription, self).setUp()
-        self.task_model = self.env['project.task']
-        self.sale_model = self.env['sale.order']
-        self.account_model = self.env['account.analytic.account']
-        self.project_model = self.env['project.project']
-        self.event_model = self.env['event.event']
-        self.task_model = self.env['project.task']
-        self.procurement_model = self.env['procurement.order']
-        self.wiz_add_model = self.env['wiz.event.append.assistant']
-        self.registration_model = self.env['event.registration']
-        account_vals = {'name': 'account procurement service project',
-                        'date_start': '2025-01-15',
-                        'date': '2025-02-28'}
-        self.account = self.account_model.create(account_vals)
-        project_vals = {'name': 'project 1',
-                        'analytic_account_id': self.account.id}
-        self.project = self.project_model.create(project_vals)
-        service_product = self.env.ref('product.product_product_consultant')
-        service_product.write({'performance': 5.0,
-                               'recurring_service': True})
-        service_product.performance = 5.0
-        service_product.route_ids = [
-            (6, 0,
-             [self.ref('procurement_service_project.route_serv_project')])]
-        sale_vals = {
-            'name': 'sale order 1',
-            'partner_id': self.ref('base.res_partner_1'),
-            'partner_shipping_id': self.ref('base.res_partner_1'),
-            'partner_invoice_id': self.ref('base.res_partner_1'),
-            'pricelist_id': self.env.ref('product.list0').id,
-            'project_id': self.account.id,
-            'project_by_task': 'no'}
-        sale_line_vals = {
-            'partner_id': self.ref('base.res_partner_1'),
-            'product_id': service_product.id,
-            'name': service_product.name,
-            'product_uom_qty': 7,
-            'product_uos_qty': 7,
-            'product_uom': service_product.uom_id.id,
-            'price_unit': service_product.list_price,
-            'performance': 5.0,
-            'january': True,
-            'february': True,
-            'week4': True,
-            'week5': True,
-            'monday': True,
-            'tuesday': True,
-            'wednesday': True,
-            'thursday': True,
-            'friday': True,
-            'saturday': True,
-            'sunday': True,
-            'start_date': '2025-01-15',
-            'start_hour': 8.00,
-            'end_date': '2025-02-28',
-            'end_hour': 12.00}
-        sale_vals['order_line'] = [(0, 0, sale_line_vals)]
-        self.sale_order = self.sale_model.create(sale_vals)
-        account_vals = {'name': 'account procurement service project',
-                        'date_start': '2025-01-15',
-                        'date': '2025-02-28'}
-        self.account2 = self.account_model.create(account_vals)
-        project_vals = {'name': 'project 1',
-                        'analytic_account_id': self.account2.id}
-        self.project2 = self.project_model.create(project_vals)
-        service_product = self.env.ref('product.product_product_consultant')
-        service_product.write({'performance': 5.0,
-                               'recurring_service': True})
-        service_product.performance = 5.0
-        service_product.route_ids = [
-            (6, 0,
-             [self.ref('procurement_service_project.route_serv_project')])]
-        sale_vals = {
-            'name': 'sale order 2',
-            'partner_id': self.ref('base.res_partner_1'),
-            'partner_shipping_id': self.ref('base.res_partner_1'),
-            'partner_invoice_id': self.ref('base.res_partner_1'),
-            'pricelist_id': self.env.ref('product.list0').id,
-            'project_id': self.account2.id,
-            'project_by_task': 'yes'}
-        sale_line_vals = {
-            'partner_id': self.ref('base.res_partner_1'),
-            'product_id': service_product.id,
-            'name': service_product.name,
-            'product_uom_qty': 7,
-            'product_uos_qty': 7,
-            'product_uom': service_product.uom_id.id,
-            'price_unit': service_product.list_price,
-            'performance': 5.0,
-            'january': True,
-            'february': True,
-            'week4': True,
-            'week5': True,
-            'monday': True,
-            'tuesday': True,
-            'wednesday': True,
-            'thursday': True,
-            'friday': True,
-            'saturday': True,
-            'sunday': True,
-            'start_date': '2025-01-15',
-            'start_hour': 8.00,
-            'end_date': '2025-02-28',
-            'end_hour': 12.00}
-        sale_vals['order_line'] = [(0, 0, sale_line_vals)]
-        self.sale_order2 = self.sale_model.create(sale_vals)
+        for line in self.sale_order.mapped('order_line').filtered(
+                lambda l: l.product_id.recurring_service):
+            line.courses = 'Courses test for description'
 
     def test_event_group_description(self):
+        self.sale_order.order_line.write({
+            'monday': True,
+            'wednesday': True,
+            'friday': True,
+            'saturday': True,
+            'sunday': True,
+        })
         self.sale_order.action_button_confirm()
-        cond = [('project_id', '=', self.project.id)]
-        event = self.event_model.search(cond, limit=1)
-        self.assertNotEqual(
-            len([event]), 0, 'Sale order withour event')
-        event._count_sale_lines()
-        event.show_sale_lines()
-        event.sale_order_line.button_group_description()
-        event.sale_order_line._compute_line_days()
+        cond = [('sale_order', '=', self.sale_order.id)]
+        events = self.event_model.search(cond)
+        self.assertNotEquals(
+            len([events]), 0, 'Sale order without event')
+        self.assertEquals(
+            sum(events.mapped('count_sale_lines')), 0)
+
+    def test_event_group_description_by_task(self):
+        self.sale_order.project_by_task = 'yes'
+        self.sale_order.action_button_confirm()
+        cond = [('sale_order', '=', self.sale_order.id)]
+        events = self.event_model.search(cond)
+        self.assertNotEquals(
+            len([events]), 0, 'Sale order without event')
+        self.assertEquals(
+            sum(events.mapped('count_sale_lines')),
+            len(self.sale_order.order_line))
+        for event in events.filtered('sale_order_line'):
+            result = event.show_sale_lines()
+            domain = result.get('domain')
+            self.assertIn(
+                domain[0][2], self.sale_order.mapped('order_line').ids)
