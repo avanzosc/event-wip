2acf49334da333ab94ac91b72a1a7ec5f98f0159
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-09-05 12:22:59 +0200

    [IMP] event_registration_analytic: Create analytic account from "Confirm assistant" wizard.

--- event_registration_analytic/wizard/__init__.py
+++ event_registration_analytic/wizard/__init__.py
@@ -3,3 +3,4 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import wiz_event_append_assistant
 from . import wiz_impute_in_presence_from_session
+from . import wiz_event_confirm_assistant
