fac18615c9efaa6a351c8e896c7343e1336a83ac
oihane <oihanecruce@gmail.com>
2016-11-11 11:30:05 +0100

    [IMP] event_track_assistant: store fields in order to make searching easier

--- event_track_assistant/tests/test_event_track_assistant.py
+++ event_track_assistant/tests/test_event_track_assistant.py
@@ -286,7 +286,7 @@ class TestEventTrackAssistant(common.TransactionCase):
     def test_event_track_assistant_delete(self):
         self.assertEquals(len(self.event.mapped('registration_ids')), 0)
         self.assertEquals(self.partner.session_count, 0)
-        self.assertEquals(self.partner.presences_count, 0)
+        self.assertEquals(self.partner.presence_count, 0)
         add_wiz = self.wiz_add_model.with_context(
             active_ids=self.event.ids).create({
                 'partner': self.partner.id,
@@ -294,7 +294,7 @@ class TestEventTrackAssistant(common.TransactionCase):
         add_wiz.action_append()
         self.assertNotEquals(len(self.event.mapped('registration_ids')), 0)
         self.assertNotEquals(self.partner.session_count, 0)
-        self.assertNotEquals(self.partner.presences_count, 0)
+        self.assertNotEquals(self.partner.presence_count, 0)
         self.assertIn(
             self.partner.id,
             self.event.mapped('registration_ids.partner_id').ids,
