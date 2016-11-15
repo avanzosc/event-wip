ff36b7e133d25af9c957461d7eabd4c6bd3c5111
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-11-15 15:01:42 +0100

    [IMP] event_registrations_analytic: Change analytic account name, where change registration event.

---
+++ event_registration_analytic/wizard/wiz_registration_to_another_event.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models
+
+
+class WizRegistrationToAnotherEvent(models.TransientModel):
+    _inherit = 'wiz.registration.to.another.event'
+
+    def _change_registration_event(self):
+        wiz_append_obj = self.env['wiz.event.append.assistant']
+        super(WizRegistrationToAnotherEvent, self)._change_registration_event()
+        if self.event_registration_id.analytic_account:
+            vals = wiz_append_obj._prepare_data_for_account_not_employee(
+                self.new_event_id, self.event_registration_id)
+            if vals.get('code', False):
+                vals.pop('code')
+            self.event_registration_id.analytic_account.write(vals)
