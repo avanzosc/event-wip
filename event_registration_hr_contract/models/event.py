1a99a25d6d277cab9eebd18b8be82cc9c3616fa8
Oihane Crucelaegui <oihanecruce@gmail.com>
2017-01-16 21:43:00 +0100

    [IMP] event_registration_hr_contract: remove functions from event

--- event_registration_hr_contract/models/event.py
+++ event_registration_hr_contract/models/event.py
@@ -2,43 +2,43 @@
 # (c) 2016 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from openerp import models, fields, api, exceptions, _
+from openerp.addons.event_track_assistant._common import\
+    _convert_to_local_date, _convert_to_utc_date, _convert_time_to_float
+
+datetime2str = fields.Datetime.to_string
+date2str = fields.Date.to_string
 
 
 class EventEvent(models.Model):
     _inherit = 'event.event'
 
     @api.depends('date_begin')
-    def _calculate_date_begin_without_hour(self):
-        for event in self:
-            event.date_begin_without_hour = False
-            if event.date_begin:
-                new_date = event._convert_date_to_local_format_with_hour(
-                    event.date_begin).strftime('%Y-%m-%d')
-                event.date_begin_without_hour = new_date
+    def _compute_date_begin_without_hour(self):
+        tz = self.env.user.tz
+        for event in self.filtered('date_begin'):
+            event.date_begin_without_hour =\
+                _convert_to_local_date(event.date_begin, tz=tz)
 
     @api.depends('date_end')
-    def _calculate_date_end_without_hour(self):
-        event_obj = self.env['event.event']
-        for event in self:
-            event.date_end_without_hour = False
-            if event.date_end:
-                new_date = event_obj._convert_date_to_local_format_with_hour(
-                    event.date_end).strftime('%Y-%m-%d')
-                event.date_end_without_hour = new_date
+    def _compute_date_end_without_hour(self):
+        tz = self.env.user.tz
+        for event in self.filtered('date_end'):
+            event.date_end_without_hour =\
+                _convert_to_local_date(event.date_end, tz=tz)
 
     date_begin_without_hour = fields.Date(
-        'Date begin without hour', store=True,
-        compute='_calculate_date_begin_without_hour')
+        string='Date begin without hour', store=True,
+        compute='_compute_date_begin_without_hour')
     date_end_without_hour = fields.Date(
-        'Date end without hour', store=True,
-        compute='_calculate_date_end_without_hour')
+        string='Date end without hour', store=True,
+        compute='_compute_date_end_without_hour')
 
 
 class EventRegistration(models.Model):
     _inherit = 'event.registration'
 
 #    @api.depends('date_start', 'date_end', 'partner_id')
-    def _calculate_contracts_permited(self):
+    def _compute_contracts_permitted(self):
         for registration in self:
             registration.contracts_permitted = [(6, 0, [])]
             if (registration.date_start and registration.date_end and
@@ -57,7 +57,7 @@ class EventRegistration(models.Model):
         string='Employee', store=True)
     contracts_permitted = fields.Many2many(
         'hr.contract', string='Contracts permitted',
-        compute='_calculate_contracts_permited')
+        compute='_compute_contracts_permitted')
 
     @api.onchange('partner_id')
     def _onchange_partner(self):
@@ -90,56 +90,57 @@ class EventRegistration(models.Model):
     @api.onchange('contract')
     def _onchange_contract(self):
         self.ensure_one()
-        event_obj = self.env['event.event']
+        tz = self.env.user.tz
         if self.contract:
             if not self.date_start:
                 self.date_start = self.event_id.date_begin
             if not self.date_end:
                 self.date_end = self.event_id.date_end
-            from_date = event_obj._convert_date_to_local_format_with_hour(
-                self.date_start).date().strftime('%Y-%m-%d')
-            if self.contract.date_start > from_date:
-                start_time = event_obj._convert_times_to_float(self.date_start)
-                self.date_start = event_obj._put_utc_format_date(
-                    self.contract.date_start, start_time)
-            to_date = event_obj._convert_date_to_local_format_with_hour(
-                self.date_end).date().strftime('%Y-%m-%d')
+            from_date = date2str(_convert_to_local_date(
+                self.date_start, tz=tz).date())
+            if self.contract.date_start and\
+                    self.contract.date_start > from_date:
+                start_time = _convert_time_to_float(self.date_start, tz=tz)
+                self.date_start = _convert_to_utc_date(
+                    self.contract.date_start, start_time, tz=tz)
+            to_date = date2str(_convert_to_local_date(
+                self.date_end, tz=tz).date())
             if self.contract.date_end and self.contract.date_end < to_date:
-                end_time = event_obj._convert_times_to_float(self.date_end)
-                self.date_end = event_obj._put_utc_format_date(
-                    self.contract.date_end, end_time)
+                end_time = _convert_time_to_float(self.date_end, tz=tz)
+                self.date_end = _convert_to_utc_date(
+                    self.contract.date_end, end_time, tz=tz)
 
     def _find_contracts_for_employee(self):
-        event_obj = self.env['event.event']
+        tz = self.env.user.tz
         permitted_contracts = self._search_permitted_contracts(self)
         self.contracts_permitted = [(6, 0, permitted_contracts.ids)]
         if len(permitted_contracts) == 1:
             self.contract = permitted_contracts[0].id
-            from_date = event_obj._convert_date_to_local_format_with_hour(
-                self.date_start).date().strftime('%Y-%m-%d')
+            from_date = date2str(_convert_to_local_date(
+                self.date_start, tz=tz).date())
             if self.contract.date_start > from_date:
-                start_time = event_obj._convert_times_to_float(self.date_start)
-                self.date_start = event_obj._put_utc_format_date(
-                    self.contract.date_start, start_time)
-            to_date = event_obj._convert_date_to_local_format_with_hour(
-                self.date_end).date().strftime('%Y-%m-%d')
+                start_time = _convert_time_to_float(self.date_start, tz=tz)
+                self.date_start = _convert_to_utc_date(
+                    self.contract.date_start, start_time, tz=tz)
+            to_date = date2str(_convert_to_local_date(
+                self.date_end, tz=tz).date())
             if self.contract.date_end and self.contract.date_end < to_date:
-                end_time = event_obj._convert_times_to_float(self.date_end)
-                self.date_end = event_obj._put_utc_format_date(
-                    self.contract.date_end, end_time)
+                end_time = _convert_time_to_float(self.date_end, tz=tz)
+                self.date_end = _convert_to_utc_date(
+                    self.contract.date_end, end_time, tz=tz)
 
     def _search_permitted_contracts(self, registration):
         contract_obj = self.env['hr.contract']
         permitted_contracts = self.env['hr.contract']
-        event_obj = self.env['event.event']
+        tz = self.env.user.tz
         if not registration.date_start:
             registration.date_start = registration.event_id.date_begin
         if not registration.date_end:
             registration.date_end = registration.event_id.date_end
-        date_start = event_obj._convert_date_to_local_format_with_hour(
-            registration.date_start).date().strftime('%Y-%m-%d')
-        date_end = event_obj._convert_date_to_local_format_with_hour(
-            registration.date_end).date().strftime('%Y-%m-%d')
+        date_start = date2str(_convert_to_local_date(
+            registration.date_start, tz=tz).date())
+        date_end = date2str(_convert_to_local_date(
+            registration.date_end, tz=tz).date())
         cond = contract_obj._search_contracts_without_date_end(
             registration.partner_id, date_end)
         contracts = contract_obj.search(cond)
@@ -174,10 +175,10 @@ class EventTrackPresence(models.Model):
 
     contract = fields.Many2one('hr.contract', string='Employee contract')
 
-    def _update_employee_calendar_days(self, contract=False,
-                                       cancel_presence=False):
+    def _update_employee_calendar_days(
+            self, contract=False, cancel_presence=False):
+        tz = self.env.user.tz
         holidays_obj = self.env['hr.holidays']
-        event_obj = self.env['event.event']
         day = self.partner_calendar_day
         if not day:
             raise exceptions.Warning(
@@ -195,9 +196,8 @@ class EventTrackPresence(models.Model):
                 absence_type = day.absence_type_from_employee_contract
                 day.write({'festive': True,
                            'absence_type': absence_type.id})
-        presence_date = event_obj._put_utc_format_date(
-            self.session_date_without_hour, 0.0).strftime(
-            '%Y-%m-%d %H:%M:%S')
+        presence_date = datetime2str(_convert_to_utc_date(
+            self.session_date_without_hour, time=0.0, tz=tz))
         cond = [('employee_id', '=', self.partner.id),
                 ('date_from', '>=', presence_date),
                 ('date_to', '<=', presence_date),
