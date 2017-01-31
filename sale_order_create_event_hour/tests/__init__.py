437c5bceb08f8ea0c6e690adef60460a21f4338e
oihane <oihanecruce@gmail.com>
2017-01-31 14:28:35 +0100

    [IMP] sale_order_create_event_hour: two test files (#184)

--- sale_order_create_event_hour/tests/__init__.py
+++ sale_order_create_event_hour/tests/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
-# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# Copyright © 2016 Alfredo de la Fuente - AvanzOSC
+# Copyright © 2017 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import test_sale_order_create_event_hour
+from . import test_type_hour
