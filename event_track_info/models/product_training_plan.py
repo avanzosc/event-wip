47c0604291e42b85742c8c4315c019c65d71a643
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-04-10 08:49:46 +0200

    [IMP] Load training plan from product template, and product variant.

--- event_track_info/models/product_training_plan.py
+++ event_track_info/models/product_training_plan.py
@@ -8,8 +8,39 @@ class ProductTrainingPlan(models.Model):
     _inherit = 'product.training.plan'
 
     def _search_product_training_plan(self, product, num_session):
-        training = self.search([
-            '|', '&', ('product_tmpl_id', '=', product.product_tmpl_id.id),
-            ('product_id', '=', False), ('product_id', '=', product.id),
-            ('sequence', '=', num_session)], limit=1)
-        return training
+        data = {}
+        cond = [('product_tmpl_id', '=', product.product_tmpl_id.id),
+                ('product_id', '=', False),
+                ('sequence', '=', num_session)]
+        data = self._catch_training_plan_information(
+            data, self.search(cond))
+        cond = [('product_tmpl_id', '=', product.product_tmpl_id.id),
+                ('product_id', '=', product.id),
+                ('sequence', '=', num_session)]
+        data = self._catch_training_plan_information(
+            data, self.search(cond))
+        return data
+
+    def _catch_training_plan_information(self, data, trainings):
+        for training in trainings:
+            plan = training.training_plan_id
+            if data.get('name', False):
+                if plan.name not in data.get('name'):
+                    data['name'] = u'{}\n\n{}'.format(
+                        data.get('name'), plan.name)
+            else:
+                data['name'] = training.training_plan_id.name
+            data['url'] = (data.get('url', False) and u'{}\n\n{}'.format(
+                data.get('url'), plan.url) or plan.url)
+            data['html_info'] = (
+                data.get('html_info', False) and u'{}\n\n{}'.format(
+                    data.get('html_info'), plan.html_info) or plan.html_info)
+            data['planification'] = (
+                data.get('planification', False) and u'{}\n\n{}'.format(
+                    data.get('planification'),
+                    plan.planification) or plan.planification)
+            data['resolution'] = (
+                data.get('resolution', False) and u'{}\n\n{}'.format(
+                    data.get('resolution'),
+                    plan.resolution) or plan.resolution)
+        return data
