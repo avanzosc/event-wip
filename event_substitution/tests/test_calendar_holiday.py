007f17b513e69f76bfb5a79294153f42f7c65066
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-11-14 16:05:00 +0100

    [IMP] event_substitution: Create a new thread when a substitution is made.

--- event_substitution/tests/test_calendar_holiday.py
+++ event_substitution/tests/test_calendar_holiday.py
@@ -2,6 +2,7 @@
 # (c) 2016 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 import openerp.tests.common as common
+from openerp import exceptions, fields
 
 
 class TestCalendarHoliday(common.TransactionCase):
@@ -13,6 +14,9 @@ class TestCalendarHoliday(common.TransactionCase):
         self.contract_model = self.env['hr.contract']
         self.calendar_model = self.env['res.partner.calendar']
         self.wiz_model = self.env['wiz.calculate.workable.festive']
+        self.holidays_model = self.env['hr.holidays']
+        self.substitution_model = self.env['wiz.event.substitution']
+        self.today = fields.Date.from_string(fields.Date.today())
         calendar_line_vals = {
             'date': '2016-01-06',
             'absence_type': self.ref('hr_holidays.holiday_status_comp')}
@@ -26,15 +30,18 @@ class TestCalendarHoliday(common.TransactionCase):
                          self.ref('hr_contract.hr_contract_type_emp'),
                          'wage': 500,
                          'date_start': '2016-01-02',
-                         'date_end': '2016-12-30',
                          'holiday_calendars':
                          [(6, 0, [self.calendar_holiday.id])]}
         self.contract = self.contract_model.create(contract_vals)
+        self.env.ref('hr.employee').address_home_id = (
+            self.ref('base.public_partner'))
+        self.env.ref('base.public_partner').employee_id = (
+            self.ref('hr.employee'))
 
     def test_calendar_holiday(self):
         self.calendar_holiday.lines[0].write({'date': '2016-01-06'})
         wiz = self.wiz_model.with_context(
-            {'active_id': self.contract.id}).create({'year': 2016})
+            {'active_id': self.contract.id}).create({'year': self.today.year})
         vals = ['year']
         wiz.with_context(
             {'active_id': self.contract.id}).default_get(vals)
@@ -42,11 +49,87 @@ class TestCalendarHoliday(common.TransactionCase):
             {'active_id':
              self.contract.id}).button_calculate_workables_and_festives()
         cond = [('partner', '=', self.ref('base.public_partner')),
-                ('year', '=', 2016)]
+                ('year', '=', self.today.year)]
         calendar = self.calendar_model.search(cond)
         self.assertNotEqual(
             len(calendar), 0, 'Calendar not generated for partner')
-        self.contract.write({'partner': self.ref('base.res_partner_1')})
+        self.partner = self.env['res.partner'].create({
+            'name': 'Partner',
+        })
+        self.user = self.env['res.users'].create({
+            'partner_id': self.partner.id,
+            'login': 'user',
+            'password': 'pass',
+        })
+        employee_vals = {
+            'name': 'Test Employee',
+            'user_id': self.user.id,
+            'address_home_id': self.partner.id
+        }
+        employee_vals.update(
+            self.env['hr.employee'].onchange_user(
+                user_id=employee_vals['user_id'])['value'])
+        self.employee = self.env['hr.employee'].create(employee_vals)
+        self.partner.employee_id = self.employee.id
+        contract_vals = {'name': 'Contract 2',
+                         'employee_id': self.employee.id,
+                         'partner': self.partner.id,
+                         'type_id':
+                         self.ref('hr_contract.hr_contract_type_emp'),
+                         'wage': 500,
+                         'date_start': '2016-01-02',
+                         'holiday_calendars':
+                         [(6, 0, [self.calendar_holiday.id])]}
+        self.contract2 = self.contract_model.create(contract_vals)
+        wiz = self.wiz_model.with_context(
+            {'active_id': self.contract2.id}).create({'year': self.today.year})
+        vals = ['year']
+        wiz.with_context(
+            {'active_id': self.contract2.id}).default_get(vals)
+        wiz.with_context(
+            {'active_id':
+             self.contract2.id}).button_calculate_workables_and_festives()
+        event = self.env.ref('event.event_0')
+        reg_vals = {
+            'partner_id': self.ref('base.public_partner'),
+            'name': 'aaaaaaa',
+            'date_start': event.date_begin,
+            'date_end': event.date_end,
+            'contract': self.contract.id,
+            'employee': self.ref('hr.employee')}
+        event.registration_ids = [(0, 0, reg_vals)]
+        hr_holidays_vals = {'name': 'Employee holidays',
+                            'holiday_type': 'employee',
+                            'holiday_status_id':
+                            self.ref('hr_holidays.holiday_status_comp'),
+                            'employee_id': self.ref('hr.employee'),
+                            'date_from': '2016-01-01 00:00:00',
+                            'date_to': '2025-01-01 00:00:00',
+                            'number_of_days_temp': 1}
+        hr_holidays = self.holidays_model.create(hr_holidays_vals)
+        wiz_vals = {
+            'holiday': hr_holidays.id,
+            'lines': [(0, 0, {'confirm_registration': True,
+                              'event': event.id,
+                              'employee': self.employee.id})]}
+        wiz = self.substitution_model.create(wiz_vals)
+        wiz._validate_employee_contract_and_registration(wiz.lines[0])
+        wiz._validate_employee_calendar(
+            self.today.year, self.today.year, self.partner)
+        wiz.substitution_employee_from_thread()
+        with self.assertRaises(exceptions.MissingError):
+            wiz.button_substitution_employee_from_thread()
+        cond = [('partner', '=', hr_holidays.employee_id.address_home_id.id),
+                ('year', '=', self.today.year)]
+        calendar = self.calendar_model.search(cond)
+        calendar.unlink()
+        with self.assertRaises(exceptions.Warning):
+            wiz.button_substitution_employee()
+        with self.assertRaises(exceptions.Warning):
+            wiz._validate_employee_contract_and_registration(wiz.lines[0])
+        self.contract2.unlink()
+        with self.assertRaises(exceptions.Warning):
+            wiz._validate_employee_contract_and_registration(wiz.lines[0])
 
     def test_wiz_event_registration_confirm(self):
         cond = [('event_id', '!=', False),
