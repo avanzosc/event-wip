fedd962bcb05e93bd3c91b37e8bf3eef9b02d78d
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-12-20 15:52:45 +0100

    [NEW] sale_order_renovate_event: New scheduler for renovate sale, sale contract, and event.

---
+++ sale_order_renovate_event/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Renovate Event",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
+    ],
+    "category": "Human Resources",
+    "depends": [
+        "sale_order_renovate_contract",
+        "sale_order_create_event"
+    ],
+    "data": [
+        "data/sale_order_renovate_event_data.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_order_renovate_event/models/sale_order.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def automatic_renovate_contract_event(self):
+        sale_orders = self.env['sale.order']
+        date = '{}-12-31'.format(
+            int(fields.Date.from_string(fields.Date.today()).year)-1)
+        cond = [('project_id', '!=', False),
+                ('project_id.state', '=', 'open'),
+                ('project_id.type', '=', 'contract'),
+                ('project_id.recurring_invoices', '=', True),
+                ('project_id.date', '=', date)]
+        sales = self.search(cond)
+        for sale in sales:
+            cond = [('sale_order', '=', sale.id)]
+            event = self.env['event.event'].search(cond, limit=1)
+            if event:
+                sale_orders += sale
+        for sale in sale_orders:
+            try:
+                sale._renovate_sale_and_contract_from_wizard()
+                cond = [('generated_from_sale_order', '=', sale.id),
+                        ('order_line', '!=', False)]
+                new_sale = self.env['sale.order'].search(cond, limit=1)
+                new_sale.action_button_confirm()
+            except Exception:
+                continue
