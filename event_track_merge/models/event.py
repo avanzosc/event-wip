89619af08cc4760e5e2006c0a3174d56582c4454
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-01-05 12:57:08 +0100

    [FIX] event_track_merge: Fix sessions merge.

--- event_track_merge/models/event.py
+++ event_track_merge/models/event.py
@@ -13,24 +13,34 @@ class EventEvent(models.Model):
         for event in self.filtered(lambda x: len(x.track_ids) > 0):
             dates = sorted(set(event.mapped('track_ids.session_date')))
             task = False
+            treated_tasks = self.env['project.task']
             tasks = self.env['project.task']
             for date in dates:
-                sessions = event.track_ids.filtered(
-                    lambda x: x.session_date == date)
+                sessions = event._take_sessions_to_date(date)
                 min_session = min(sessions, key=lambda x: x.id)
                 duration = min_session.duration
-                name = min_session.name
                 task = min_session.tasks
+                name = task[0].name
+                description = task[0].description
+                if task:
+                    if task[0] not in treated_tasks:
+                        treated_tasks += task[0]
                 others_sessions = sessions.filtered(
                     lambda x: x.id != min_session.id).sorted(
                     key=lambda e: e.id)
                 for other_session in others_sessions:
                     duration += other_session.duration
-                    name = u'{}\n{}'.format(name, other_session.name)
-                    if (other_session.tasks and
-                        other_session.tasks[0] not in tasks and
-                            other_session.tasks[0] != task):
-                        tasks += other_session.tasks[0]
+                    if other_session.tasks:
+                        if (other_session.tasks[0] not in tasks and
+                            other_session.tasks[0] not in treated_tasks and
+                                other_session.tasks[0] != task):
+                            tasks += other_session.tasks[0]
+                        if other_session.tasks[0].name not in name:
+                            name = u'{} - {}'.format(
+                                name, other_session.tasks[0].name)
+                            description = u'{}\n{}'.format(
+                                description,
+                                other_session.tasks[0].description)
                     for presence in other_session.presences:
                         p = min_session.presences.filtered(
                             lambda x: x.partner == presence.partner)
@@ -40,23 +50,19 @@ class EventEvent(models.Model):
                             p.write({'real_duration': (
                                 p.real_duration + presence.real_duration)})
                             presence.unlink()
+                task[0].write({'name': name, 'description': description})
                 others_sessions.unlink()
                 min_session.write({'name': name,
                                    'duration': duration})
             if tasks:
-                name = task.name
-                description = task.description
-                for t in tasks:
-                    name = u'{} - {}'.format(name, t.name)
-                    description = u'{}\n{}'.format(description,
-                                                   t.description)
-                    if t.service_project_sale_line:
-                        t.service_project_sale_line.write(
-                            {'service_project_task': task.id})
-                task.write({'name': name,
-                            'description': description})
-                cond = [('task_id', 'in', tasks.ids)]
-                works = work_obj.search(cond)
-                if works:
-                    works.write({'task_id': task.id})
-                tasks.unlink()
+                for t in tasks.filtered(lambda x: x.id not in
+                                        treated_tasks.ids):
+                    cond = [('task_id', '=', t.id)]
+                    works = work_obj.search(cond)
+                    if works:
+                        works.write({'task_id': task.id})
+                    t.unlink()
+
+    def _take_sessions_to_date(self, date):
+        sessions = self.track_ids.filtered(lambda x: x.session_date == date)
+        return sessions
--- sale_order_create_event/wizard/wiz_event_confirm_assistant.py
+++ sale_order_create_event/wizard/wiz_event_confirm_assistant.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from openerp import fields, models
 from openerp.addons.event_track_assistant._common import\
-    _convert_to_local_date, _convert_to_utc_date
+    _convert_to_local_date
 
 datetime2str = fields.Datetime.to_string
 
@@ -18,12 +18,10 @@ class WizEventConfirmAssistant(models.TransientModel):
         append_vals = super(WizEventConfirmAssistant,
                             self)._prepare_data_confirm_assistant(reg)
         from_date = _convert_to_local_date(reg.date_start, tz=tz).date()
-        from_date = datetime2str(_convert_to_utc_date(from_date, 0.0, tz=tz))
         to_date = _convert_to_local_date(reg.date_end, tz=tz).date()
-        to_date = datetime2str(_convert_to_utc_date(to_date, 0.0, tz=tz))
         cond = [('event_id', '=', reg.event_id.id),
-                ('date', '>=', from_date),
-                ('date', '<=', to_date),
+                ('session_date', '>=', from_date),
+                ('session_date', '<=', to_date),
                 ('date', '!=', False)]
         sessions = session_obj.search(cond)
         tasks = sessions.mapped('tasks')
