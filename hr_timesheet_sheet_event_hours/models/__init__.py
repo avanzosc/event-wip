3b76d5372381b4dcda26b3ba4aebfe8508075943
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-04-09 16:26:26 +0200

    [IMP] hr_timesheet_sheet_event_hours: Calculate hours worked, and show holidays in imputations.

--- hr_timesheet_sheet_event_hours/models/__init__.py
+++ hr_timesheet_sheet_event_hours/models/__init__.py
@@ -2,3 +2,4 @@
 # Copyright © 2018 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import hr_timesheet_sheet_sheet
+from . import hr_analytic_timesheet
